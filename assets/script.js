a08a1be43a65a7235656aa523567f4b86ecdd8da
Nick D <Ndefected15@gmail.com>
2022-01-02 20:23:34 -0500

    forecast needs work

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,171 @@
-var locationInput = document.querySelector("#city");
-var searchInput = document.querySelector("#search");
+function init() {
+  const inputEl = document.getElementById("city-input");
+  const searchEl = document.getElementById("search-btn");
+  const nameEl = document.getElementById("city-name");
+  const currentPicEl = document.getElementById("current-pic");
+  const currentTempEl = document.getElementById("temp");
+  const currentHumidityEl = document.getElementById("humidity");
+  const currentWindEl = document.getElementById("wind");
+  const currentUVEl = document.getElementById("UV");
+  const clearEl = document.getElementById("clear-history");
+  const historyEl = document.getElementById("history");
+  let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+  console.log(searchHistory);
 
-var getCurrentWeather = async (event) => {
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput.value.trim()}&appid=2cbf1a9e40ee9174bee7a4b6e1685a82`;
-  event.preventDefault();
-  const res = await fetch(apiUrl);
-  let data = await res.json();
-  console.log(data);
-  drawWeather(data);
-};
+  function getWeather(cityName) {
+    // using cityName get the current condition from the API
+    let apiUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&appid=2cbf1a9e40ee9174bee7a4b6e1685a82";
+    axios
+      .get(apiUrl)
 
-function drawWeather(d) {
-  var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
-  var fahrenheit = Math.round((parseFloat(d.main.temp) - 273.15) * 1.8 + 32);
+      .then(function (response) {
+        console.log(response);
+        const currentDate = new Date(response.data.dt * 1000);
+        console.log(currentDate);
+        const day = currentDate.getDate();
+        const month = currentDate.getMonth() + 1;
+        const year = currentDate.getFullYear();
+        nameEl.innerHTML =
+          response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+        let weatherPic = response.data.weather[0].icon;
+        currentPicEl.setAttribute(
+          "src",
+          "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png"
+        );
+        currentPicEl.setAttribute("alt", response.data.weather[0].description);
+        currentTempEl.innerHTML =
+          "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+        currentHumidityEl.innerHTML =
+          "Humidity: " + response.data.main.humidity + "%";
+        currentWindEl.innerHTML =
+          "Wind Speed: " + response.data.wind.speed + " MPH";
+        let lat = response.data.coord.lat;
+        let lon = response.data.coord.lon;
+        let UVQueryURL =
+          "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" +
+          lat +
+          "&lon=" +
+          lon +
+          "&appid=2cbf1a9e40ee9174bee7a4b6e1685a82&cnt=1";
+        axios.get(UVQueryURL).then(function (response) {
+          let UVIndex = document.createElement("span");
+          UVIndex.setAttribute("class", "badge badge-danger");
+          UVIndex.innerHTML = response.data[0].value;
+          currentUVEl.innerHTML = "UV Index: ";
+          currentUVEl.append(UVIndex);
+        });
+        //  Using saved city name, execute a 5-day forecast get request from open weather map api
+        let cityID = response.data.id;
+        let forecastQueryURL =
+          "https://api.openweathermap.org/data/2.5/forecast?id=" +
+          cityID +
+          "&appid=2cbf1a9e40ee9174bee7a4b6e1685a82";
+        axios
+          .get(forecastQueryURL)
 
-  // document.getElementById("description").innerHTML = d.weather[0].description;
+          .then(function (response) {
+            //  Parse response to display forecast for next 5 days underneath current conditions
+            console.log(response);
+            const forecastEls = document.querySelectorAll(".forecast");
+            for (i = 0; i < forecastEls.length; i++) {
+              forecastEls[i].innerHTML = "";
+              const forecastIndex = i * 8 + 4;
+              const forecastDate = new Date(
+                response.data.list[forecastIndex].dt * 1000
+              );
+              const forecastDay = forecastDate.getDate();
+              const forecastMonth = forecastDate.getMonth() + 1;
+              const forecastYear = forecastDate.getFullYear();
+              const forecastDateEl = document.createElement("p");
+              forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
+              forecastDateEl.innerHTML =
+                forecastMonth + "/" + forecastDay + "/" + forecastYear;
+              forecastEls[i].append(forecastDateEl);
+              const forecastWeatherEl = document.createElement("img");
+              forecastWeatherEl.setAttribute(
+                "src",
+                "https://openweathermap.org/img/wn/" +
+                  response.data.list[forecastIndex].weather[0].icon +
+                  "@2x.png"
+              );
+              forecastWeatherEl.setAttribute(
+                "alt",
+                response.data.list[forecastIndex].weather[0].description
+              );
+              forecastEls[i].append(forecastWeatherEl);
+              const forecastTempEl = document.createElement("p");
+              forecastTempEl.innerHTML =
+                "Temp: " +
+                k2f(response.data.list[forecastIndex].main.temp) +
+                " &#176F";
+              forecastEls[i].append(forecastTempEl);
+              const forecastHumidityEl = document.createElement("p");
+              forecastHumidityEl.innerHTML =
+                "Humidity: " +
+                response.data.list[forecastIndex].main.humidity +
+                "%";
+              forecastEls[i].append(forecastHumidityEl);
+            }
+          });
+      });
+  }
 
-  document.getElementById("location").innerHTML = d.name;
-  document.getElementById("temp").innerHTML = fahrenheit + "&deg;";
-  document.getElementById("wind").innerHTML = d.wind.speed;
-  document.getElementById("humidity").innerHTML = d.main.humidity;
-  // document.getElementById("UV").innerHTML = d.
+  searchEl.addEventListener("click", function () {
+    const searchTerm = inputEl.value;
+    getWeather(searchTerm);
+    searchHistory.push(searchTerm);
+    localStorage.setItem("search", JSON.stringify(searchHistory));
+    renderSearchHistory();
+  });
+
+  clearEl.addEventListener("click", function () {
+    searchHistory = [];
+    renderSearchHistory();
+  });
+
+  function k2f(K) {
+    return Math.floor((K - 273.15) * 1.8 + 32);
+  }
+
+  function renderSearchHistory() {
+    historyEl.innerHTML = "";
+    for (let i = 0; i < searchHistory.length; i++) {
+      const historyItem = document.createElement("input");
+      // <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"></input>
+      historyItem.setAttribute("type", "text");
+      historyItem.setAttribute("readonly", true);
+      historyItem.setAttribute("class", "form-control d-block bg-white");
+      historyItem.setAttribute("value", searchHistory[i]);
+      historyItem.addEventListener("click", function () {
+        getWeather(historyItem.value);
+      });
+      historyEl.append(historyItem);
+    }
+  }
+
+  renderSearchHistory();
+  if (searchHistory.length > 0) {
+    getWeather(searchHistory[searchHistory.length - 1]);
+  }
 }
 
-// window.onload = function () {
-// getCurrentWeather(6167865);
-//};
+init();
 
-searchInput.addEventListener("submit", getCurrentWeather);
+// function drawWeather(d) {
+//   var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
+//   var fahrenheit = Math.round((parseFloat(d.main.temp) - 273.15) * 1.8 + 32);
+
+//   // document.getElementById("description").innerHTML = d.weather[0].description;
+
+//   document.getElementById("location").innerHTML = d.name;
+//   document.getElementById("temp").innerHTML = fahrenheit + "&deg;";
+//   document.getElementById("wind").innerHTML = d.wind.speed;
+//   document.getElementById("humidity").innerHTML = d.main.humidity;
+// }
+
+// window.onload = function () {
+// getWeather(6167865);
+//};
